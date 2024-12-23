67710de352009297cace0f7740c158be29234aeb
xiaxianlin <dreamtimefly@gmail.com>
2024-12-23 22:30:54 +0800

    vconsole

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import VConsole from "vconsole";
 import { DemoApp } from "@apps/demo";
 
 import "./index.css";
+
+new VConsole();
 
 const root = document.getElementById("root")!;
 ReactDOM.createRoot(root).render(
