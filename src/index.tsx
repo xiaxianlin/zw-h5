ca64eb7fef2b145546f9e585745719bd93573ec8
xiaxianlin <dreamtimefly@163.com>
2024-12-25 16:34:16 +0800

    remove vconsole

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import VConsole from "vconsole";
 import { Texture } from "@apps/texture";
 
 import "./index.css";
-
-new VConsole();
 
 const doc = document.documentElement;
 doc.style.fontSize = (doc.clientWidth / 750) * 100 + "px";
