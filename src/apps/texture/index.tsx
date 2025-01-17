73b3783fdd4534da1fcf9854148abf8014c5d580
xiaxianlin <dreamtimefly@163.com>
2025-01-17 09:52:36 +0800

    share

--- src/apps/texture/index.tsx
+++ src/apps/texture/index.tsx
@@ -3,11 +3,15 @@ import { Loading } from "./Loading";
 import { AppModel } from "@apps/texture/model";
 import Content from "./Content";
 import Music from "./Music";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
+import { setShareInfo } from "@shared/utils";
 
 const Modal = lazy(() => import("./Modal"));
 
 export function Texture() {
+  useEffect(() => {
+    setShareInfo();
+  }, []);
   return (
     <AppModel.Provider>
       <Loading />
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,5 +1,33 @@
+import axios from "axios";
+
+declare var wx: any;
+
 export const sleep = (duration: number) => {
   return new Promise((resolve) => {
     setTimeout(resolve, duration);
+  });
+};
+
+export const setShareInfo = async () => {
+  const shareUrl = window.location.href;
+  const sharePic = "https://zw.ixxl.me/textures/modal/x01.png";
+  const res = await axios({
+    method: "post",
+    url: "https://dgs.cztv.com/apix/khsv/cbk/jkt",
+    data: { shareUrl, sharePic },
+  });
+  if (wx && !res?.data?.data) return;
+  wx.config({ ...res.data.data });
+  wx.ready(() => {
+    console.log(">>>>> wx.ready", res);
+    wx.updateAppMessageShareData({
+      title: "宋韵祥纹",
+      desc: "快来一起定制你的专属祥纹吧",
+      link: shareUrl,
+      imgUrl: sharePic,
+      success: function () {
+        console.log("updateAppMessageShareData success");
+      },
+    });
   });
 };
